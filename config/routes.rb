70903d159b8b4e47c06834f4473031d1818f300f
Juan Pablo Cuevas <jp.cuevaslavin@gmail.com>
2017-08-16 18:17:56 -0300

    Added forms css - Added user relation to post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-	
-  resources :posts
-  devise_for :users
 
-  root 'posts#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	resources :posts
+	devise_for :users, controllers: {
+		sessions: 'users/sessions',
+		registrations: 'users/registrations'
+	}
+
+	root 'posts#index'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
